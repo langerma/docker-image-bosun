957724c579383374ef622e3d8e4c1569f218d61e
Markus Langer <langer.markus@gmail.com>
2018-12-20 11:15:09 +0000

    small fix

--- docker/start_bosun.sh
+++ docker/start_bosun.sh
@@ -4,6 +4,6 @@ BOSUN=/opt/bosun/bin/bosun
 CONFIG_FILE=/etc/bosun/bosun.conf
 BOSUN_VERSION=$(${BOSUN} -version)
 
-echo "Starting Bosun"
+echo "Starting Bosun..."
 ${BOSUN} -c ${CONFIG_FILE} -t && ${BOSUN} -c ${CONFIG_FILE}
 
