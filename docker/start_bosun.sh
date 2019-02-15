d08df4f8afcc906cf744318b7c07d577b0996669
Markus Langer <langer.markus@gmail.com>
2019-02-15 10:29:29 +0100

    updated bosun

--- docker/start_bosun.sh
+++ docker/start_bosun.sh
@@ -4,6 +4,6 @@ BOSUN=/opt/bosun/bin/bosun
 CONFIG_FILE=/etc/bosun/bosun.conf
 BOSUN_VERSION=$(${BOSUN} -version)
 
-echo "Starting Bosun..."
+echo "Starting bosun version 0.8.0-dev last modified 2019-02-15T10:25:34+01:00"
 ${BOSUN} -c ${CONFIG_FILE} -t && ${BOSUN} -c ${CONFIG_FILE}
 
